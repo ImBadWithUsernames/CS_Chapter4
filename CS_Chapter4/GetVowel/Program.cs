7625bd16ff438bf365e7cfe021272da6b780e719
ImBadWithUsernames <antoniogonz2001@gmail.com>
2017-02-16 14:06:06 -0500

    Did a bit in GetVowel and Sum

--- CS_Chapter4/GetVowel/Program.cs
+++ CS_Chapter4/GetVowel/Program.cs
@@ -10,6 +10,15 @@ namespace GetVowel
     {
         static void Main(string[] args)
         {
+            Console.Write("Please name a vowel in caps. Type '!' to end.");
+            String input = Console.ReadLine().ToUpper();
+            while (input != "!")
+            {
+                while (input == "E" || "U" || "I" || "O" || "A")
+                    Console.WriteLine("OK");
+                Console.WriteLine("ERROR");
+            }
+
         }
     }
 }
--- CS_Chapter4/Sum/Program.cs
+++ CS_Chapter4/Sum/Program.cs
@@ -10,6 +10,21 @@ namespace Sum
     {
         static void Main(string[] args)
         {
+            Console.Write("  ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("+ ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            int sum = number1 + number2;
+            while (sum < 999)
+            {
+                Console.Write("+ ");
+                int number3 = Convert.ToInt32(Console.ReadLine());
+                int sum2 = sum + number3;
+                Console.WriteLine("= {0}", sum2);
+
+
+            }
+                
         }
     }
 }

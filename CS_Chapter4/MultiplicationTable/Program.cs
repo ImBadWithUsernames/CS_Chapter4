1fa9a993b4a40d6d474783375ba80d67b6eba518
ImBadWithUsernames <antoniogonz2001@gmail.com>
2017-02-24 14:16:25 -0500

    Did half of MultiplicationTable
    stilldontknowwhatimdoing

--- CS_Chapter4/MultiplicationTable/Program.cs
+++ CS_Chapter4/MultiplicationTable/Program.cs
@@ -10,6 +10,19 @@ namespace MultiplicationTable
     {
         static void Main(string[] args)
         {
+            int x;
+            int y;
+            const int limit = 10;
+            x = 1;
+            y = 1;
+            int z = x * y;
+            while (x <= limit)
+            {
+                Console.WriteLine("{0} * {1} = {2}.",x,y,z);
+                ++x;
+                z = x * y;
+            }
+            for (x = 1;y = 1, x <= limit; ++x) ;
         }
     }
 }
